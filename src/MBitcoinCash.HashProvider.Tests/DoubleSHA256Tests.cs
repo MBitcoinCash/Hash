4e9876ad34a36bcb7afca2d7dba158b8301524c7
Vladas Cibulskis <vladas.cibulskis@gmail.com>
2018-07-08 22:20:02 +0300

    GH-1 Adding additional test for DoubleSHA256 hash provider.

--- src/MBitcoinCash.HashProvider.Tests/DoubleSHA256Tests.cs
+++ src/MBitcoinCash.HashProvider.Tests/DoubleSHA256Tests.cs
@@ -15,7 +15,7 @@ namespace MBitcoinCash.HashProvider.Tests
     public class DoubleSHA256Tests
     {
         /// <summary>
-        /// Checks that calculated has is correct.
+        /// Checks that calculated hash is correct.
         /// </summary>
         /// <remarks>
         /// Test is based on the Bitcoin transaction: https://blockchain.info/tx/74d350ca44c324f4643274b98801f9a023b2b8b72e8e895879fd9070a68f7f1f?format=hex
@@ -26,6 +26,26 @@ namespace MBitcoinCash.HashProvider.Tests
         {
             var inputString = "02000000019b69251560ea1143de610b3c6630dcf94e12000ceba7d40b136bfb67f5a9e4eb000000006b483045022100a52f6c484072528334ac4aa5605a3f440c47383e01bc94e9eec043d5ad7e2c8002206439555804f22c053b89390958083730d6a66c1b711f6b8669a025dbbf5575bd012103abc7f1683755e94afe899029a8acde1480716385b37d4369ba1bed0a2eb3a0c5feffffff022864f203000000001976a914a2420e28fbf9b3bd34330ebf5ffa544734d2bfc788acb1103955000000001976a9149049b676cf05040103135c7342bcc713a816700688ac3bc50700".ToLowerInvariant();
             var hashString = "74d350ca44c324f4643274b98801f9a023b2b8b72e8e895879fd9070a68f7f1f";
+
+            var input = inputString.TransactionHexToByteArray();
+
+            var hashProvider = new DoubleSHA256();
+            var computedHash = hashProvider.ComputeHash(input);
+            Assert.AreEqual(hashString, computedHash.ToHexTransactionId());
+        }
+
+        /// <summary>
+        /// Checks that calculated hash is correct.
+        /// </summary>
+        /// <remarks>
+        /// Test is based on Bitcoin documentation sample: https://en.bitcoin.it/wiki/Protocol_documentation#Differential_encoding
+        /// </remarks>
+        [TestMethod]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "String is in hex format, so it does not have any characters affected by conversion to lower.")]
+        public void CalculateDoubleSHA256Hash1()
+        {
+            var inputString = "hello";
+            var hashString = "9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50";
 
             var input = inputString.ToByteArray();
 
--- src/MBitcoinCash.HashProvider.Tests/StringToByteArrayConverter.cs
+++ src/MBitcoinCash.HashProvider.Tests/StringToByteArrayConverter.cs
@@ -8,6 +8,7 @@ namespace MBitcoinCash.HashProvider.Tests
 {
     using System;
     using System.Linq;
+    using System.Text;
 
     /// <summary>
     /// Converts string representation of byte array to real byte array.
@@ -19,9 +20,34 @@ namespace MBitcoinCash.HashProvider.Tests
         /// </summary>
         /// <param name="input">The input.</param>
         /// <returns>The byte array.</returns>
-        internal static byte[] ToByteArray(this string input)
+        internal static byte[] TransactionHexToByteArray(this string input)
         {
             return Enumerable.Range(0, input.Length / 2).Select(x => Convert.ToByte(input.Substring(x * 2, 2), 16)).ToArray();
+        }
+
+        /// <summary>
+        /// To the byte array.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>The byte array.</returns>
+        internal static byte[] ToByteArray(this string input)
+        {
+            return Encoding.ASCII.GetBytes(input);
+        }
+
+        /// <summary>
+        /// To the hexadecimal string.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>The string representation of byte array.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "This is hash in hex format, so it does not have any characters affected by conversion to lower.")]
+        internal static string ToHexTransactionId(this byte[] input)
+        {
+            var reversedArray = new byte[input.Length];
+            input.CopyTo(reversedArray, 0);
+            Array.Reverse(reversedArray);
+
+            return BitConverter.ToString(reversedArray).Replace("-", string.Empty, StringComparison.InvariantCulture).ToLowerInvariant();
         }
 
         /// <summary>
@@ -32,11 +58,7 @@ namespace MBitcoinCash.HashProvider.Tests
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "This is hash in hex format, so it does not have any characters affected by conversion to lower.")]
         internal static string ToHexString(this byte[] input)
         {
-            var reversedArray = new byte[input.Length];
-            input.CopyTo(reversedArray, 0);
-            Array.Reverse(reversedArray);
-
-            return BitConverter.ToString(reversedArray).Replace("-", string.Empty, StringComparison.InvariantCulture).ToLowerInvariant();
+            return BitConverter.ToString(input).Replace("-", string.Empty, StringComparison.InvariantCulture).ToLowerInvariant();
         }
     }
 }
